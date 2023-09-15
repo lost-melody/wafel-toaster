b7246159fd567fff73fa90e5f784d0cb42fd9a6c
lost-melody <lost__melody@163.com>
2023-09-15 15:41:36 +0800

    use time instead of chrono

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
-    let now = chrono::Utc::now().format("%Y%m%d%H%M");
+    let format = time::format_description::parse_borrowed::<2>("[year][month][day][hour][minute]").unwrap();
+    let now = time::OffsetDateTime::now_utc().format(&format).unwrap();
     println!("cargo:rustc-env=VERSION={now}");
 }
